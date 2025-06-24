273963fa9b749061c3092156620be72ad429cbc2
Chris Kuhar <chriskuhar@gmail.com>
2025-06-23 20:58:56 -0700

    display message in shadow dom, insulate app from message CSS

--- yasma-client/src/app/main/MessageView.tsx
+++ yasma-client/src/app/main/MessageView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import useMessageFormat from "@/hooks/UseMessageFormat";
 import UseApi from "@/hooks/UseApi";
 import {useMailStore} from "@/stores/mail-store";
@@ -6,12 +6,31 @@ import {useMailStore} from "@/stores/mail-store";
 export function MessageView() {
   const curMessage = useMailStore((state) => state.messageState);
   const { messageRender } = useMessageFormat();
-  const [ message, setMessage ] = useState(null);
   const { getMessage } = UseApi();
+  const shadowRootRef = useRef(null);
   useEffect(() => {
     async function fetchData(messageID: string) {
       const result = await getMessage(messageID);
-      setMessage(messageRender(result));
+      const rawMessage = messageRender(result);
+      const parser = new DOMParser();
+      const targetElement = shadowRootRef.current;
+      const messageDoc = parser.parseFromString(rawMessage, 'text/html');
+
+      let wrapper = targetElement.querySelector(".shadow-wrapper");
+
+      if (wrapper) {
+          // If wrapper exists, remove it to remove the Shadow DOM root
+          targetElement.removeChild(wrapper);
+      }
+
+      // Re-attach the new Shadow DOM
+      wrapper = document.createElement("div");
+      wrapper.className = "shadow-wrapper";
+      targetElement.appendChild(wrapper);
+
+      // Create a shadow root
+      const shadowRoot = wrapper.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(messageDoc.documentElement);
     }
     const curMessageID = curMessage?.MessageID;
     if(curMessageID) {
@@ -21,7 +40,8 @@ export function MessageView() {
 
   return (
       <>
-        <div dangerouslySetInnerHTML={{__html: message}}/>
+        <div ref={shadowRootRef}>
+        </div>
       </>
   );
 }
